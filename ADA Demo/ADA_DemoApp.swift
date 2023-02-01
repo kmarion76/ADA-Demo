2cdba931e3ba45140acfe92b54e4e6b8f53b3975
kmarion76 <kmarion76@gmail.com>
2023-02-01 09:24:07 -0500

    Initial Commit

---
+++ ADA Demo/ADA_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADA_DemoApp.swift
+//  ADA Demo
+//
+//  Created by Kevin Marion on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ADA_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
